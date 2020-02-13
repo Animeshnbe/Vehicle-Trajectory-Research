66cad60541bc32364347c271f01597aa5532f084
Animeshnbe <35167280+Animeshnbe@users.noreply.github.com>
2020-02-13 17:12:55 +0530

    Dataset read/write and preprocess
    -Foursquare TSMC dataset: NYC and Tokyo
    -t-drive taxi dataset: Beijing
    -Go-tracks dataset: Brazil (sparse and noisy)
    -Geolife dataset: Microsoft Beijing (huge)

--- dataread0.py
+++ dataread0.py
@@ -1,5 +1,36 @@
-'''
+import csv
+####################EARTH DISTANCE FINDER#####################
+from math import radians, sin, cos, asin, sqrt
+def distfind(src,dest):
+    slon,slat = src[0],src[1]
+    elon,elat = dest[0],dest[1]
+    slat=radians(float(slat))
+    slon=radians(float(slon))
+    elat=radians(float(elat))
+    elon=radians(float(elon))
+    dlat=elat-slat
+    dlon=elon-slon
+    a = sin(dlat/2)**2 + cos(slat)*cos(elat) * sin(dlon/2)**2
+    #c = 2 * atan2(sqrt(a), sqrt(1-a))
+    dist=2*6371010*asin(sqrt(a))
+    return dist
+    #API based
+    #gmaps = googlemaps.Client(key='AIzaSyANek40q60bomgvZAYTs5sOSrMZTh9AFn8')
+    #print(gmaps.distance_matrix((40.714224, -73.961452),(116.43003,39.93209))['rows'][0]['elements'][0]['distance']['value'])
+    
+from sklearn.metrics import pairwise_distances
+def diff_affinity(X):
+    return pairwise_distances(X, metric=distfind)
+
+#######################GET TIME INTERVAL (b/w consecutive readings)####################
 from datetime import datetime
+def timeint(start, end):
+    sdt = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+    edt = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+    timel=(edt-sdt).total_seconds()
+    return timel
+
+###############READ TSMC DATASET WITH TIMESTAMP ORDERING IN CSV####################
 import os
 path="C:/Users/mayank/Downloads/ANIMESH/datasets/dataset_tsmc2014/"
 trlist=[]
@@ -15,80 +46,108 @@ for file in os.scandir(path):
             #print(coord)
             trlist.append([float(row[4]),float(row[5]),dt])
 
-from math import radians, sin, cos, asin, sqrt
-def distfind(src,dest):
-    slon,slat = src[0],src[1]
-    elon,elat = dest[0],dest[1]
-    #print(slon)
-    slat=radians(float(slat))
-    slon=radians(float(slon))
-    elat=radians(float(elat))
-    elon=radians(float(elon))
-    dlat=elat-slat
-    dlon=elon-slon
-    a = sin(dlat/2)**2 + cos(slat)*cos(elat) * sin(dlon/2)**2
-    #c = 2 * atan2(sqrt(a), sqrt(1-a))
-    dist=2*6371010*asin(sqrt(a))
-    return dist
-    #gmaps = googlemaps.Client(key='AIzaSyANek40q60bomgvZAYTs5sOSrMZTh9AFn8')
-    #print(gmaps.distance_matrix((40.714224, -73.961452),(116.43003,39.93209))['rows'][0]['elements'][0]['distance']['value'])
-    
-from sklearn.metrics import pairwise_distances
-def diff_affinity(X):
-    return pairwise_distances(X, metric=distfind)
-
+########################AGGLOMERATIVE CLUSTERING FOR ALL POINTS###############
 from sklearn.cluster import AgglomerativeClustering
 clusters = AgglomerativeClustering(n_clusters=None, affinity=diff_affinity, compute_full_tree='auto', connectivity=None, distance_threshold=100.0, linkage='complete')
 clusters.fit(trlist[:2]) 
 y=clusters.labels_
-'''
 
-import csv
-trajlist=[]
-ct=0
-dataf=open("C:/Users/Animesh/Documents/vta research project/matched-trajectories.csv", 'r')
-rd=csv.reader(dataf, delimiter=',', quoting=csv.QUOTE_NONE)
-for row in rd:
-    if ct%2==0:
-        xt=[]
-        i=0
-        while i<len(row) and row[i]!="":
-        #for i in range(0,len(row),2):
-            try:
-                lon=float(row[i][3:-1])
-                lat=float(row[i+1][2:-3])
-            except ValueError as e:
-                print(ct, i)
-                break
-            xt.append([lat,lon])
-            i+=2
-        trajlist.append(xt)
-    ct+=1
-'''
-import folium
-import random
-def createmap(trajlist,i):
-    my_map = folium.Map(location = [39.907388,116.397013], zoom_start = 11.5)  
-    for xt in trajlist:
-        if len(xt)!=0:
-            loc=[tuple(l) for l in xt]
-            random_number = random.randint(0,16777215)
-            hex_number = str(hex(random_number))
-            hex_number ='#'+ hex_number[2:]
-            folium.PolyLine(locations = loc, color=hex_number, weight=1.5, line_opacity = 0.5).add_to(my_map)
-     
-    my_map.save("C:/Users/Animesh/Documents/vta research project/my_map"+str(i)+".html")
+#######################READ GEOLIFE DATASET###################################
+from pathlib import Path
+import numpy as np
 
-createmap(trajlist,0)
-'''
-#MAP_MATCHING
+path3="C:/Users/Animesh/Documents/vta research project/DATASETS/Geolife Trajectories 1.3/Data/"
+entry=Path(path3)
+ctr=0
+trajectories=[]
+coords=[]
+for direc in entry.iterdir():
+    #print(path3+direc.name)
+    for x in os.scandir(path3+direc.name+"/Trajectory/"):
+        trlist=[]
+        with open(x, mode='r') as dtfile:
+            rd=csv.reader(dtfile,delimiter=',')
+            ct=0
+            for row in rd:
+                ct+=1
+                if ct<7:
+                    continue
+                trlist.append([row[0],row[1],row[4]])
+                coords.append([float(row[0]),float(row[1])])
+        if len(trlist)>3:
+            trajectories.append(trlist)
+#Range of the dataset
+bounds=[[min(coords[:,0]),min(coords[:,1])],[max(coords[:,0]),max(coords[:,1])]]
+
+#################TIME DISTRIBUTION IN GEOLIFE DATASET##################
+times=[]
+for direc in entry.iterdir():
+    for x in os.scandir(path3+direc.name+"/Trajectory/"):
+        with open(x, mode='r') as dtfile:
+            rd=csv.reader(dtfile,delimiter=',')
+            ct=0
+            for row in rd:
+                ct+=1
+                if ct<7:
+                    continue
+                times.append(int(row[6][:2]))
+                
+time_ranges = np.zeros(24, dtype='uint8')
+for t in times:
+    time_ranges[t]+=1
+
+import matplotlib.pyplot as plt
+
+y_pos = np.arange(24)
+plt.bar(y_pos, time_ranges, align='center', alpha=0.5)
+plt.xticks(np.arange(0, 24, 2))
+plt.ylabel('Distribution')
+plt.title('Cars by time of day')
+plt.show()
+
+#######################READ TSMC DATASET (NYC AND TOKYO)######################    
+path="C:/Users/mayank/Downloads/ANIMESH/datasets/dataset_tsmc2014/"
+trlist=[]
+userlist=[]
+for file in os.scandir(path):
+    with open(file, 'r') as c_file:
+        cread=csv.reader(c_file,delimiter='\t')
+        lines=0
+        for row in cread:
+            userlist.append(float(row[0]))
+            stx=row[7]
+            st=stx.replace('+0000 ','')
+            dt=datetime.strptime(st,'%a %b %d %H:%M:%S %Y')
+            #coord=row[4].split('\t')
+            #print(coord)
+            trlist.append([float(row[4]),float(row[5]),dt])
+            
+from collections import Counter
+lab=Counter(userlist).keys()
+print(len(lab))
+
+############################GO TRACKS BRAZIL DATASET##############################
+path2="C:/Users/mayank/Downloads/ANIMESH/datasets/GPS Trajectory/"
+import pandas as pd
+df=pd.read_csv(path2+'go_track_trackspoints.csv')
+trl=df.groupby('track_id').apply(lambda x: x[['latitude','longitude']].values.astype('float32').tolist())
+
+trlist=[]
+for i in trl:
+    g=[]
+    for j in range(len(i)):
+        g.append([float(i[j][0]),float(i[j][1])])
+    trlist.append(g)
+    
+
+###############################MAP MATCHING####################################
+##############################BING MAPS API####################################
 modtrlist=[]
 import requests
-for i in trajlist:
+for i in trlist:
     d={}
     pointslist=[] 
     for j in i:
-        #print(j[0])
         d["latitude"]= float(j[0])
         d["longitude"]= float(j[1])
         pointslist.append(d.copy())
@@ -106,7 +165,7 @@ for i in trajlist:
         #else:
          #   modtrlist.append(i)
         
-#createmap(modtrlist,1)
+##############################MAPBOX API####################################
 from mapbox import MapMatcher
 def mapmatch(coords):
     b=coords.tolist()
@@ -156,88 +215,57 @@ def floatify(coordinates):
     return coords
 
 modtrlist1=[]
-for i in range(len(trajlist)):
-    modtrlist1.append(mapmatch(floatify(trajlist[i])))
+for i in range(len(trlist)):
+    modtrlist1.append(mapmatch(floatify(trlist[i])))
+
+###############################WRITE BACK PROCESSED DATA##########################    
+import csv
+with open("filename.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(trlist)
+#np.savetxt('trajectories.csv',np.array(trlist),delimiter=',')
+    
 with open('trajmod.csv', mode='w') as dtfile:
     wr=csv.writer(dtfile, delimiter=',')
     for i in range(len(modtrlist)):
         wr.writerow(modtrlist[i])
-
-#np.savetxt('trajectories.csv',np.array(trlist),delimiter=',')
-
-def split_sequences(trlist, n_steps_in, n_steps_out):
-    X, y = list(), list()
-    for sequences in trlist:
-        for i in range(len(sequences)):
-            sequences=np.array(sequences)
-            # find the end of this pattern
-            end_ix = i + n_steps_in
-            out_end_ix = end_ix + n_steps_out
-            # check if we are beyond the dataset
-            if out_end_ix > len(sequences):
+    
+####################READ PROCESSED DATA (MATCHED TRAJECTORIES)###################
+import csv
+trajlist=[]
+ct=0
+dataf=open("C:/Users/Animesh/Documents/vta research project/matched-trajectories.csv", 'r')
+rd=csv.reader(dataf, delimiter=',', quoting=csv.QUOTE_NONE)
+for row in rd:
+    if ct%2==0:
+        xt=[]
+        i=0
+        while i<len(row) and row[i]!="":
+        #for i in range(0,len(row),2):
+            try:
+                lon=float(row[i][3:-1])
+                lat=float(row[i+1][2:-3])
+            except ValueError as e:
+                print(ct, i)
                 break
-        	# gather input and output parts of the pattern
-            seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix:out_end_ix, :]
-            X.append(seq_x)
-            y.append(seq_y)
-    return np.array(X), np.array(y)
-
-x_train, y_train=split_sequences(modtrlist, 3, 1)
-n_features = x_train.shape[2]
-
-import keras
-#from keras.layers import BatchNormalization, Add, Flatten, Activation
-from keras.models import Model, Sequential, Input
-from keras.layers.recurrent import GRU, LSTM
-from keras.layers.core import Dense, Masking
-#from sklearn.metrics import classification_report
-#from keras.optimizers import Nadam
-#from keras import regularizers
-from keras.layers import TimeDistributed, Embedding, Bidirectional, RepeatVector
-#from keras.utils import to_categorical
-# define model
-model = Sequential()
-model.add(LSTM(20, activation='relu', input_shape=(3, n_features)))
-model.add(RepeatVector(1))
-model.add(Bidirectional(LSTM(20, activation='relu', return_sequences=True)))
-model.add(TimeDistributed(Dense(2)))
-model.compile(optimizer='Adamax', loss='mse', metrics=['accuracy'])
-# fit model
-from keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
-model.fit(x_train,y_train, epochs=10, verbose=1, 
-          validation_split=0.3, 
-          batch_size=500, 
-          shuffle=True,
-          callbacks=[es])
-
-x_input = np.array([[39.93366,116.4557],[39.9425,116.45912],[39.94288,116.45537]])
-x_input = x_input.reshape((1, 3, n_features))
-yhat = model.predict(x_input, verbose=0)
-
-def get_model():
-    #density
-    model=Sequential()
-    #model.add(Embedding(len(lab), 16, input_length=ts, batch_input_shape=(1,ts)))
-    #model.add(Masking(mask_value=0., batch_input_shape=(1,ts,1)))
-    model.add(Bidirectional(LSTM(482, input_shape=(482, 1), return_sequences=True)))
-    #model.add(GRU(20, implementation=1, activity_regularizer=None, return_sequences=True))
-    model.add(TimeDistributed(Dense(1503, activation='softmax')))
-    #model.add(Dense(1503, activation='softmax'))
-    #model.add(Flatten(batch_input_shape=(1,)))
-    #model.add(Activation('softmax'))
-    print(model.summary())
-
-    model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='Adamax', metrics = ['accuracy'])
-    return model
-
-model=get_model()
-
-from keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
-model.fit(x_train,y_train, epochs=10, verbose=1, 
-          validation_split=0.3, 
-          batch_size=4, 
-          shuffle=True,
-          callbacks=[es])
+            xt.append([lat,lon])
+            i+=2
+        trajlist.append(xt)
+    ct+=1
+    
+#######################READ CELL SEQUENCES WITHOUT REPETITIONS###################
+##########################BAD LOGIC AS WITHOUT TIMESTAMP#########################
+ctr=0
+s=0
+mt=[]
+for i in range(len(x)):
+    modtraj=[]
+    s=s+len(x[i])
+    while ctr<s:
+        modtraj.append(y[ctr])
+        #remove no change cell sequences
+        while ctr+1<s and y[ctr]==y[ctr+1]:
+            ctr+=1
+        ctr+=1
+    if len(modtraj)>1:
+        mt.append(modtraj)
